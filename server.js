b71843d91b3e8bb8c8d0eb5cb5c84aee7e524bfa
Alanv3514 <alanv3514@gmail.com>
2023-09-25 01:26:17 -0300

    swagger doc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require('express');
-const router = require("./src/Routes/index.js")
-
+const router = require("./src/Routes/index.js");
+const {swaggerDocs: v1SwaggerDocs} = require("./src/Controller/swagger.js");
 const app = express();
 
 app.use(express.json());
@@ -9,6 +9,7 @@ app.use(express.json());
 const START = (port) => {
     app.use("/api", router);
     app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    v1SwaggerDocs(app, port);
     return app;
 }
 module.exports = START;
--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,18 +1,126 @@
-const exp = require('constants');
 const User = require('../Model/user.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
-exports.getAll = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-    };
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: API para usuarios
+ */
 
+/**
+ * @swagger
+ *
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *         - role
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: ID generado automáticamente por MongoDB
+ *         name:
+ *           type: string
+ *           description: Nombre del usuario
+ *         email:
+ *           type: string
+ *           description: Correo electrónico del usuario
+ *         password:
+ *           type: string
+ *           description: Contraseña del usuario
+ *         role:
+ *           type: string
+ *           description: Rol del usuario
+ *       example:
+ *         name: John Doe
+ *         email: johndoe@example.com
+ *         password: password123
+ *         role: user
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Obtiene todos los usuarios
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Error al obtener los usuarios
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensaje de error
+ */
+
+
+
+exports.getAll = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+/**
+* @swagger
+* /api/users/register:
+*   post:
+*     summary: Crea un nuevo usuario
+*     tags: [Users]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       201:
+*         description: Usuario creado
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*       400:
+*         description: Error al crear el usuario
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               properties:
+*                 error:
+*                   type: string
+*                   description: Mensaje de error
+*/
 exports.create = async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(saltRounds);
@@ -29,22 +137,67 @@ exports.create = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
-//creamos el login y devolvemos un jwt
-exports.login =  async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
 
-    if (!user) {
-        return res.status(400).send('Usuario no encontrado');
-    }
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     summary: Inicia sesión y devuelve un token JWT
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Correo electrónico del usuario
+ *               password:
+ *                 type: string
+ *                 description: Contraseña del usuario
+ *     responses:
+ *       200:
+ *         description: Token JWT
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: Token JWT
+ *       400:
+ *         description: Error al iniciar sesión
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensaje de error
+ */
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (!isPasswordCorrect) {
-        return res.status(400).send('Contraseña incorrecta');
-    }
+  if (!user) {
+    return res.status(400).send('Usuario no encontrado');
+  }
 
-    const token = jwt.sign({ _id: user._id, role: user.role, email: user.email  }, process.env.JWT_SECRET);
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-    res.send({ token });
+  if (!isPasswordCorrect) {
+    return res.status(400).send('Contraseña incorrecta');
+  }
+
+  const token = jwt.sign({ _id: user._id, role: user.role, email: user.email }, process.env.JWT_SECRET);
+
+  res.send({ token });
 };
