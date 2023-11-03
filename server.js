27270cd6fb9592869c510dca92e8f34b90033dcc
evinzon <evinzon@CAJAJPER.GOV.AR>
2023-11-03 09:41:47 -0300

    +add cors

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = require("./src/Routes/index.js");
 const {swaggerDocs: v1SwaggerDocs} = require("./src/Controller/swaggerController.js");
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
-
+app.use(cors());
 const START = (port) => {
     app.use("/api", router);
     app.listen(port, () => console.log(`Example app listening on port ${port}!`));
